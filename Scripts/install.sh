4c819afbd2e33d29af57ddbaa31d4f7fb47c916f
Graygzou <gregoire.boiron@gmail.com>
2018-05-26 19:22:33 -0400

    Change the install.sh file.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -8,13 +8,31 @@ BASE_URL=http://netstorage.unity3d.com/unity
 
 # We need that version 2017.3.1f1 here
 HASH=fc1d3344e6ea
-VERSION=5.4.1f1
+VERSION=2017.3.1f1
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
-echo 'Downloading from https://download.unity3d.com/download_unity/fc1d3344e6ea/Windows64EditorInstaller/UnitySetup64-2017.3.1f1.exe: '
-curl -o Unity.exe https://download.unity3d.com/download_unity/fc1d3344e6ea/Windows64EditorInstaller/UnitySetup64-2017.3.1f1.exe
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.exe -target /
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
