b14dbdcdd7119e67de81be6e4aac13a75c0be165
Graygzou <gregoire.boiron@gmail.com>
2018-05-27 18:45:16 -0400

    Add tests and changed .gitignore a bit.

---
+++ Assets/Dossiers Personnels/Greg/Scripts/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClass {
+
+    private int id;
+    private bool isAlive;
+
+    public TestClass(int id)
+    {
+        this.id = id;
+        isAlive = true;
+    }
+
+    public void Death()
+    {
+        isAlive = false;
+    }
+
+    public bool IsAlive()
+    {
+        return isAlive;
+    }
+
+    public void Revive(int team)
+    {
+        isAlive = true;
+    }
+}
---
+++ Assets/Editor/NewEditModeReal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeReal {
+
+	[Test]
+	public void AliveWhenCreated() {
+        TestClass testObj = new TestClass(1);
+        Assert.AreEqual(true, testObj.IsAlive());
+    }
+
+
+    [Test]
+    public void DeadWhenKilled()
+    {
+        TestClass testObj = new TestClass(1);
+        testObj.Death();
+        Assert.AreEqual(false, testObj.IsAlive());
+    }
+
+    [Test]
+    public void AliveWhenRevive()
+    {
+        int team = 0;
+
+        TestClass testObj = new TestClass(1);
+        testObj.Death();
+        Assert.AreEqual(false, testObj.IsAlive());
+        testObj.Revive(team);
+        Assert.AreEqual(true, testObj.IsAlive());
+    }
+}
