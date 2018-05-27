65d3234d01c7673016bd10861c6c1e4a825fad3d
Graygzou <gregoire.boiron@gmail.com>
2018-05-27 19:33:11 -0400

    change tests file.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -3,16 +3,18 @@
 # This file is based on an example by SebastianJay. See the original example here:
 #	https://github.com/SebastianJay/unity-ci-test/
 
+project = $(pwd)/Assets/$1
+
 ## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
+echo "Running editor unit tests for $project"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-projectPath "$project" \
+  -editorTestsResultFile $(pwd)/test-results.xml \
 	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test-results.xml \
 	-quit
 
 results=$?
@@ -23,8 +25,9 @@ cat $(pwd)/test-results.xml
 # Exit if tests failed
 if [ $results -ne 0 ]
 then
-  echo "Failed unit tests";
-  exit $results;
+  echo "Failed unit tests. Exited with $results"
+else
+  echo "All tests passed. Exited with $results"
 fi
 
 set +e
