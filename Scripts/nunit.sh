4faff158650a213ae74eb68768f91d971f2c5b3e
Graygzou <gregoire.boiron@gmail.com>
2018-05-29 16:59:03 -0400

    Give a shot to OpenCover.

--- Scripts/nunit.sh
+++ Scripts/nunit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 runTest() {
-    mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+    mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.4/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
    if [ $? -ne 0 ]
    then
      exit 1
---
+++ Scripts/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+OPENCOVER=$(pwd)/coverage/OpenCover.4.5.519/OpenCover.Console.exe
+
+coverage="./coverage"
+rm -rf $coverage
+mkdir $coverage
+
+# Execute the command
+echo "Calculating coverage with OpenCover"
+$OPENCOVER \
+  -target:"$(pwd)/packages/NUnit.ConsoleRunner.2.6.4/tools/nunit3-console.exe" \
+  -targetargs:"./Library/ScriptAssemblies/Assembly-CSharp.dll" \
+  -mergeoutput \
+  -hideskipped:File \
+  -output:$coverage/coverage.xml \
+  -oldStyle \
+  -filer:"+[Brain-Control*]*" \
+  -searchdirs:"./" \
+  -register:user
+
+# Test with NUnit 2
