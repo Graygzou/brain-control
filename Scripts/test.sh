86709d81baab3497764d740a18bfbe70369930fc
Graygzou <gregoire.boiron@gmail.com>
2018-05-30 02:25:52 -0400

    Fix OpenCover path.

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -5,7 +5,7 @@ coverage="./coverage"
 # Execute the command
 # OpenCover is meant to be executed on Windows... Fail.
 echo "Calculating coverage with OpenCover"
-mono $(pwd)/coverage/OpenCover.4.6.519/tools/OpenCover.Console.exe \
+mono $(pwd)/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe \
   -target:"$(pwd)/testrunner/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe" \
   -targetargs:"/nologo /noshadow $(pwd)/Library/ScriptAssemblies/Assembly-CSharp.dll" \
   -output:$coverage/coverage.xml \
@@ -19,7 +19,7 @@ cat $coverage/coverage.xml
 echo ""
 
 echo "v2 !"
-mono $(pwd)/coverage/OpenCover.4.6.519/tools/OpenCover.Console.exe \
+mono $(pwd)/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe \
   -target:"$(pwd)/testrunner/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe" \
   -targetargs:"/nologo /noshadow $(pwd)/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll" \
   -output:$coverage/coverage2.xml \
