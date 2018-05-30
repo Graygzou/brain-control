0b4778c6c156a09498fc37e0ccbbb88f7ba3f958
Graygzou <gregoire.boiron@gmail.com>
2018-05-30 00:51:05 -0400

    change filter arg from opencover.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -19,7 +19,9 @@ echo "Running editor unit tests for $project"
 results=$?
 
 echo "Unit test logs"
+echo ""
 cat $(pwd)/unit-test-results.xml
+echo ""
 
 # Exit if tests failed
 if [ $results -ne 0 ]
@@ -27,6 +29,14 @@ then
   echo "Failed unit tests. Exited with $results"
 else
   echo "All tests passed. Exited with $results"
+fi
+
+#Test for coveralls
+if [ -n "$COVERALLS_REPO_TOKEN" ]
+then
+	echo "Coveralls"
+  mono ./coveralls/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i $(pwd)/unit-test-results.xml --useRelativePaths
+	echo "Done"
 fi
 
 # Test for codecov
--- Scripts/test.sh
+++ Scripts/test.sh
@@ -6,16 +6,38 @@ coverage="./coverage"
 echo "Calculating coverage with OpenCover"
 mono $(pwd)/coverage/OpenCover.4.6.519/tools/OpenCover.Console.exe \
   -target:"$(pwd)/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe" \
-  -targetargs:"./Library/ScriptAssemblies/Assembly-CSharp-Editor-Editor.dll" \
+  -targetargs:"./Library/ScriptAssemblies/Assembly-CSharp-Editor.dll" \
   -mergeoutput \
   -hideskipped:File \
   -output:$coverage/coverage.xml \
   -oldStyle \
-  -filter:"+[Brain-Control*]* -[Brain-Control.*Tests*]*" \
+  -filter:"+[*]* -[NewEditMode*]*" \
   -searchdirs:"./" \
   -register:user
-
 echo "Done!"
+
+echo "Unit test logs"
+echo ""
+cat $coverage/coverage.xml
+echo ""
+
+echo "v2 !"
+mono $(pwd)/coverage/OpenCover.4.6.519/tools/OpenCover.Console.exe \
+  -target:"$(pwd)/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe" \
+  -targetargs:"./Library/ScriptAssemblies/Assembly-CSharp-Editor.dll" \
+  -mergeoutput \
+  -hideskipped:File \
+  -output:$coverage/coverage2.xml \
+  -oldStyle \
+  -filter:"+[*]* -[NewEditMode*]*" \
+  -searchdirs:"./" \
+  -register:user
+echo "Done2!"
+
+echo "Unit test logs2"
+echo ""
+cat $coverage/coverage2.xml
+echo ""
 
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
